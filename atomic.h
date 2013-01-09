7727881c5e19ca57637a5ec57b93587c90f6d2ea
Thomas Wihl <twihl@arri.at>
2013-01-09 15:15:32 +0100

    atomic now working on x86

--- atomic.h
+++ atomic.h
@@ -2,24 +2,48 @@
 #define ATOMIC_H
 
 #include <stdint.h>
+#if __ARM__
+//inline int32_t atomic_add(volatile int32_t* v, int inc)
+//{
+//   int32_t t;
+//   int tmp;
 
-inline int32_t atomic_add(volatile int32_t* v, int inc)
+//   __asm__ __volatile__(
+//                         "1:  ldrex   %0, [%2]        \n"
+//                         "    add     %0, %0, %3      \n"
+//                         "    strex   %1, %0, [%2]    \n"
+//                         "    cmp     %1, #0          \n"
+//                         "    bne     1b              \n"
+
+//                         : "=&r" (t), "=&r" (tmp)
+//                         : "r" (v), "r" (inc)
+//                         : "cc", "memory");
+
+//   return t;
+//}
+
+static inline void atomic_add(volatile int32_t* v, int inc)
 {
-   int32_t t;
-   int tmp;
+        unsigned long tmp;
+        int result;
 
-   __asm__ __volatile__(
-                         "1:  ldrex   %0, [%2]        \n"
-                         "    add     %0, %0, %3      \n"
-                         "    strex   %1, %0, [%2]    \n"
-                         "    cmp     %1, #0          \n"
-                         "    bne     1b              \n"
-
-                         : "=&r" (t), "=&r" (tmp)
-                         : "r" (v), "r" (inc)
-                         : "cc", "memory");
-
-   return t;
+        __asm__ __volatile__("@ atomic_add\n"
+"1:     ldrex   %0, [%3]\n"
+"       add     %0, %0, %4\n"
+"       strex   %1, %0, [%3]\n"
+"       teq     %1, #0\n"
+"       bne     1b"
+        : "=&r" (result), "=&r" (tmp), "+Qo" (*v)
+        : "r" (v), "Ir" (inc)
+        : "cc");
 }
+#endif  // ARM
+
+#if __x86_64__ || __X86_32__ || __X86__
+inline void atomic_add(volatile int32_t* v, int inc)
+{
+    *v += inc;
+}
+#endif  // X86
 
 #endif // ATOMIC_H
